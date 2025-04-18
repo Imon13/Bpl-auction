3cf87bfaf1afec77196bc2a5ba0afdcc355db8f4
imon <imonsaha153@gmail.com>
2025-04-19 05:38:04 +0600

    initial work of total and grand price are done

--- js/scripts.js
+++ js/scripts.js
@@ -24,8 +24,48 @@ for (const btn of allBtn) {
         else {
             alert("You reached max limited")
         }
+        const name = e.target.parentNode.parentNode.childNodes[1].innerText
+        console.log(name);
+        const price = e.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText
+        console.log(price);
+        const categoty = e.target.parentNode.parentNode.childNodes[5].childNodes[1].innerText
+        console.log(categoty);
+
+
+
+        const selectedArea = document.getElementById('selected')
+        const li = document.createElement('li')
+        const p = document.createElement('p')
+        const p2 = document.createElement('p')
+        const p3 = document.createElement('p')
+        p.innerText = name
+        p2.innerText = price
+        p3.innerText = categoty
+        li.appendChild(p)
+        li.appendChild(p2)
+        li.appendChild(p3)
+        selectedArea.appendChild(li)
+
+        const totalPrice = document.getElementById('total').innerText
+        console.log(totalPrice);
+
+        const convertedTotalPrice = parseInt(totalPrice)
+        console.log(convertedTotalPrice);
+
+        const newTotalPrice = convertedTotalPrice + parseInt(price)
+        console.log(newTotalPrice);
+
+        document.getElementById('total').innerText = newTotalPrice
+
+        const grandTotal = document.getElementById('grand-total').innerText
+        const convertrdGrandTotal = parseInt('grand-total')
+        const newGrandTotal = convertrdGrandTotal + parseInt(price)
+        document.getElementById('grand-total').innerText = newTotalPrice
+
 
     })
+
+
 
 }
 function setInnerText(id, value) {
