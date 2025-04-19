33ed8df1ba43d1354ae9312d8354b7229036769a
imon <imonsaha153@gmail.com>
2025-04-19 07:12:36 +0600

    compleate

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,46 @@
 let cart = 0;
 let left = 6;
+let budget = 1200;
 const allBtn = document.getElementsByClassName('allBtn')
-console.log(allBtn);
 for (const btn of allBtn) {
     btn.addEventListener('click', function (e) {
-        console.log('clik');
-
-        if (left == 0) {
-            alert('full')
+        const price = parseInt(e.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText)
+        if (left === 0) {
+            alert('Cart is full!')
+            return
         }
-        else {
-            cart = cart + 1
-
-            setInnerText('add', cart)
-
+        if (price > budget) {
+            alert('Not enough budget!')
+            return
         }
-
-        if (left > 0) {
-            left = left - 1
-            setInnerText('left', left)
-
-        }
-        else {
-            alert("You reached max limited")
-        }
+        cart = cart + 1
+        left = left - 1
+        budget = budget - price
+        setInnerText('add', cart)
+        setInnerText('left', left)
         const name = e.target.parentNode.parentNode.childNodes[1].innerText
-        console.log(name);
-        const price = e.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText
-        console.log(price);
         const categoty = e.target.parentNode.parentNode.childNodes[5].childNodes[1].innerText
-        console.log(categoty);
-
-
-
         const selectedArea = document.getElementById('selected')
-        const li = document.createElement('li')
-        const p = document.createElement('p')
-        const p2 = document.createElement('p')
-        const p3 = document.createElement('p')
-        p.innerText = name
-        p2.innerText = price
-        p3.innerText = categoty
-        li.appendChild(p)
-        li.appendChild(p2)
-        li.appendChild(p3)
-        selectedArea.appendChild(li)
-
-        const totalPrice = document.getElementById('total').innerText
-        console.log(totalPrice);
-
-        const convertedTotalPrice = parseInt(totalPrice)
-        console.log(convertedTotalPrice);
-
-        const newTotalPrice = convertedTotalPrice + parseInt(price)
-        console.log(newTotalPrice);
-
+        const row = document.createElement('div')
+        row.classList.add('flex', 'justify-around', 'mt-1', 'text-sm')
+        const nameEl = document.createElement('p')
+        nameEl.classList.add('w-1/3')
+        nameEl.innerText = name
+        const priceEl = document.createElement('p')
+        priceEl.classList.add('w-1/3')
+        priceEl.innerText = price
+        const categoryEl = document.createElement('p')
+        categoryEl.classList.add('w-1/3')
+        categoryEl.innerText = categoty
+        row.appendChild(nameEl)
+        row.appendChild(priceEl)
+        row.appendChild(categoryEl)
+        selectedArea.appendChild(row)
+        const totalPrice = parseInt(document.getElementById('total').innerText)
+        const newTotalPrice = totalPrice + price
         document.getElementById('total').innerText = newTotalPrice
-
-        const grandTotal = document.getElementById('grand-total').innerText
-        const convertrdGrandTotal = parseInt('grand-total')
-        const newGrandTotal = convertrdGrandTotal + parseInt(price)
         document.getElementById('grand-total').innerText = newTotalPrice
-
-
     })
-
-
-
 }
 function setInnerText(id, value) {
     document.getElementById(id).innerText = value
